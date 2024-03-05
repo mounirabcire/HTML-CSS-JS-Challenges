7ab5556653246e0f85f248f7b92d13ac5cc48675
mounirabcire <mounirwebdevcode@gmail.com>
2024-03-05 21:04:42 +0100

    updated

--- Expanding-cards/assets/script.js
+++ Expanding-cards/assets/script.js
@@ -1,12 +1,17 @@
+const body = document.querySelector("body");
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
     panel.addEventListener("click", () => {
-        removeActiveClass();
+        // removeActiveClass();
 
         panel.classList.add("active");
     });
 });
+
+body.addEventListener("click", () => {
+    removeActiveClass();
+}, true);
 
 function removeActiveClass() {
     panels.forEach((panel) => {
