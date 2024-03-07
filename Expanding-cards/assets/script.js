216533887a88eb35e782e9ffaae9587d95d85dc6
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-03-07 12:22:30 +0100

    Update script.js

--- Expanding-cards/assets/script.js
+++ Expanding-cards/assets/script.js
@@ -2,13 +2,13 @@ const body = document.querySelector("body");
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
+    // 1- event click will trigger first the body then the panel.
     panel.addEventListener("click", () => {
-        // removeActiveClass();
-
         panel.classList.add("active");
     });
 });
 
+// I set the third parameter(useCapture) to true, because whenever I'm gonna click on a pennel element I wanted to handle the click event first on the body which is the parent then on the panel which is the child. (capturing)   
 body.addEventListener("click", () => {
     removeActiveClass();
 }, true);
